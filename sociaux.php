f9e9af3da63ce1f39e8693b47d5299c5727b41cc
Lezarius <remy.mazabraud@gmail.com>
2014-05-11 12:37:59 +0200

    Social

--- sociaux.php
+++ sociaux.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="BootStrap/css/bootstrap.css">
 	<link rel="stylesheet" href="BootStrap/css/menu.css" />
+	<link rel="stylesheet" href="BootStrap/font-awesome/css/font-awesome.css">
 	<script src="js/jquery.js"></script>
 	<script src="js/navigation.js"></script> 
 	<script src="js/typed.js" type="text/javascript"></script>
@@ -18,9 +19,27 @@
 </div>
 
 <div class="container">
+<div class="row">
+<h3>Voici les différent endroit ou vous pouvez me retrouvez :</h3>
+</div>
+<div class="row" align="center">
 
-
-
+  <div class="col-md-4">
+  <span class="fa-stack fa-lg">
+    <a href="http://twitter.com/remy_mazabraud"><i class="fa fa-twitter-square fa-5x "></i></a>
+</span>
+</div>
+  <div class="col-md-4">
+<span class="fa-stack fa-lg">
+    <a href="https://github.com/Lezarius"><i class="fa fa-github-square fa-5x "></i></a>
+</span>
+  </div>
+  <div class="col-md-4">
+	
+<span class="fa-stack fa-lg">
+    <a href="https://bitbucket.org/lezarius"><i class="fa  fa-bitbucket-square fa-5x "></i></a>
+</span>
+  </div>
 
 
 
